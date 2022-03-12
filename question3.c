7a03b8a645003a69d6e26d4e9e5116daf8a850a8
NIKETA21 <84794703+NIKETA21@users.noreply.github.com>
2022-03-12 11:42:10 +0530

    Create question3.c

---
+++ question3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+mode_t umask(mode_t mask);
+
+
+int main(){
+	int fd;
+	unmask(022);
+	if ((fd=creat("temp",0666))==-1)
+	 perror("creat");
+	 system("ls -l temp");
+	}
